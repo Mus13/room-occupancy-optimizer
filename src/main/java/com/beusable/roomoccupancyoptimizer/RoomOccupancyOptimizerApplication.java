e034301ab0c96ee874f0986f33fa64d8032ee51c
MBELABED <Mustapha.13000@gmail.com>
2023-11-10 15:54:59 +0100

    Installing project

---
+++ src/main/java/com/beusable/roomoccupancyoptimizer/RoomOccupancyOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.beusable.roomoccupancyoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomOccupancyOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomOccupancyOptimizerApplication.class, args);
+	}
+
+}
